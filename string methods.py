a0374898f6e5a8e39321452cff87fd8a969a312f
aima-a <aimaanwar828@gmail.com>
2023-10-05 20:30:18 -0400

    --

---
+++ string methods.py
@@ -0,0 +1,23 @@
+name = "John"
+# Convert to uppercase
+upper_name = name.upper()
+print(upper_name)
+print(name.upper())
+
+# Convert to lowercase
+lower_name = name.lower()
+print(lower_name)
+
+# Remove whitespace from the beginning and end of a string
+text = " Hello World "
+print(text)
+print(text.strip())
+
+# Split string into a list of entries in the string
+text = "hello world"
+list = text.split()
+print(list)
+
+# Replace substring
+text = "apples and bananas"
+print(text.replace("apples", "oranges"))
